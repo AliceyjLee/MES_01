2da7ca1598ee416852d814db26250cf4bdd84d12
AliceyjLee <124331742+AliceyjLee@users.noreply.github.com>
2023-08-09 10:19:11 +0900

    failed pull

--- signalr_team/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace signalr_team.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
